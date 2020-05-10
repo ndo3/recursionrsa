f24c7b0e43e9d866724c3a90b4f09ce5acc6dea0
Josh Roy <joshnroy@gmail.com>
2020-05-10 15:53:23 -0400

    fixed bug in saving models

--- main.py
+++ main.py
@@ -210,8 +210,8 @@ def main(training=True, alpha = 1, output_file = None):
         print("Training Literals")
         literal_speaker_losses, literal_listener_losses = train_literals(literal_listener_training_data, literal_speaker_training_data, l0, s0, NUM_EPOCHS)
 
-        torch.save(s0.state_dict(), "literal_speaker.pth")
-        torch.save(l0.state_dict(), "literal_listener.pth")
+        torch.save(s0.state_dict(), "literal_speaker" + str(alpha) + ".pth")
+        torch.save(l0.state_dict(), "literal_listener" + str(alph) + ".pth")
 
         literal_listener_losses = np.clip(gaussian_filter1d(literal_listener_losses, sigma=smoothing_sigma), 0., clip_bound)
         literal_speaker_losses = np.clip(gaussian_filter1d(literal_speaker_losses, sigma=smoothing_sigma), 0., clip_bound)
