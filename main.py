ca458c0f944a325f49079a2fc121a2d934acc0b1
Natalie Delworth <natalie_delworth@brown.edu>
2020-05-09 23:08:09 -0400

    fixed range

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def run_reasoning(pragmatic_listener_testing_data, literal_listener, literal_spe
     result_dict = {}
     dynamic_dict = {}
     print("Num utterances: ", len(all_utterances))
-    for i in range(0, 2, 2):
+    for i in range(0, 12, 2):
         print("Max level: ", i)
         speakers, listeners = create_reasoning_entities(literal_listener, literal_speaker, all_utterances, alpha, levels_of_recursion=i)
         # print(speakers, listeners)
