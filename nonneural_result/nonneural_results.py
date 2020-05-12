9da8e20ef91dd2ad7565def9c87c9b940fe80ae1
Natalie Delworth <natalie_delworth@brown.edu>
2020-05-11 23:15:17 -0400

    more analysis scripts

--- nonneural_result/nonneural_results.py
+++ nonneural_result/nonneural_results.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import ast
 import pandas as pd
 
-results = open("result_newer.txt", "r").readlines()
+results = open("classical_results.txt", "r").readlines()
 results = [r.split("\n")[0] for r in results]
 results = [r.split("|") for r in results]
 results = [(float(a), ast.literal_eval(d)) for a,d in results]
-results = [{'alpha': [a]*6, 'recursions': list(range(0, 12, 2)), 'accuracy': [d[i] for i in range(0, 12, 2)]} for a,d in results]
+print(results)
+results = [{'alpha': [a]*6, 'recursions': list(range(0, 12, 2)), 'accuracy': [d[str(i)] for i in range(0, 12, 2)]} for a,d in results]
 results = {'alpha': [r['alpha'] for r in results],
             'recursions': [r['recursions'] for r in results],
             'accuracy': [r['accuracy'] for r in results]}
@@ -17,14 +18,14 @@ results = {'alpha': [aa for a in results['alpha'] for aa in a],
 
 
 
-df = pd.DataFrame(results)
-df['alpha_recursions'] = list(zip(df['alpha'], df['recursions']))
-df_grouped = df.groupby(['alpha_recursions']).agg('mean')
+df_grouped = pd.DataFrame(results)
+#df['alpha_recursions'] = list(zip(df['alpha'], df['recursions']))
+#df_grouped = df.groupby(['alpha_recursions']).agg('mean')
 
 print(df_grouped)
 
-palette = sns.color_palette("muted", 5)
-ax = sns.lineplot(x = "recursions", y = "accuracy", hue = 'alpha', data = df_grouped, legend = "full", palette = palette)
+
+ax = sns.lineplot(x = "recursions", y = "accuracy", hue = 'alpha', data = df_grouped, legend = "full")
 plt.ylim([0.25, 0.7])
-plt.title("Neural RSA")
+plt.title("Classical RSA")
 plt.show()
